58e561b6d100ad0fb51f807c1ba735d2e0591a68
Enmaaaaaa <83886600+Enmaaaaaa@users.noreply.github.com>
2024-10-20 18:21:17 -0500

    Update main_bm.cpp

--- benchmarks/main_bm.cpp
+++ benchmarks/main_bm.cpp
@@ -187,7 +187,7 @@ static void BM_openmp_critical2(benchmark::State& state) {
     OpenMPCritical histogramCalculatorC;
 
     for (auto _ : state) {
-        auto histogramaC = histogramCalculatorC.calculate(randomInput, MAXIMO_VALOR, NUMERO_ELEMENTOS);
+      auto histogramaC = histogramCalculatorC.calculate(randomInput, MAXIMO_VALOR, NUMERO_ELEMENTOS);
       benchmark::DoNotOptimize(histogramaC);
     }
 }
