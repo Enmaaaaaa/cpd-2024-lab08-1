63d13d8027a81defff22d862ab7d102633c45239
Enmaaaaaa <83886600+Enmaaaaaa@users.noreply.github.com>
2024-10-20 18:15:02 -0500

    Update openmp_critical.cpp

--- histograms/openmp_critical.cpp
+++ histograms/openmp_critical.cpp
@@ -1,13 +1,27 @@
 #include "openmp_critical.h"
+#include <omp.h>
 
-std::vector<int> OpenMPCritical::calculate(const int* input, const int buckets, const int size) {
-  std::vector<int> histogram(buckets, 0);
+std::vector<int> OpenMPCritical::calculate(const int* input, const int numElements, const int maxValue) {
+    std::vector<int> histogram(maxValue, 0); 
 
-  #pragma omp parallel for
-  for (int idx = 0; idx < size; idx++) {
-    #pragma omp critical
-    histogram[input[idx] - 1]++;
+    #pragma omp parallel
+    {
+        std::vector<int> localHistogram(maxValue, 0); 
+
+        #pragma omp for
+        for (int i = 0; i < numElements; ++i) {
+            if (input[i] > 0 && input[i] <= maxValue) {
+                localHistogram[input[i] - 1]++;
+            }
+        }
+
+        #pragma omp critical
+        {
+            for (int j = 0; j < maxValue; ++j) {
+                histogram[j] += localHistogram[j];
+            }
+        }
     }
 
-  return histogram;
+    return histogram;
 }
