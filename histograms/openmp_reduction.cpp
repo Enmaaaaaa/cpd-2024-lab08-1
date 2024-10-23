98952fa0053f0f683b29f19a82cf9ad07ce5186d
Enmaaaaaa <83886600+Enmaaaaaa@users.noreply.github.com>
2024-10-23 11:33:05 -0500

    Update openmp_reduction.cpp

--- histograms/openmp_reduction.cpp
+++ histograms/openmp_reduction.cpp
@@ -2,12 +2,19 @@
 #include <omp.h>
 
 std::vector<int> OpenMPReduction::calculate(const int* input, const int buckets, const int size) {
-  std::vector<int> histogram(buckets, 0);
+    std::vector<int> histogram(buckets, 0);
 
-#pragma omp parallel for reduction(+ : histogram[:buckets])
-  for (int i = 0; i < size; i++) {
-    histogram[input[i] - 1]++;
-  }
+    int* temp_histogram = new int[buckets]();
 
-  return histogram;
+    #pragma omp parallel for reduction(+: temp_histogram[:buckets])
+    for (int i = 0; i < size; i++) {
+        temp_histogram[input[i] - 1]++;
+    }
+
+    for (int j = 0; j < buckets; j++) {
+        histogram[j] += temp_histogram[j];
+    }
+
+    delete[] temp_histogram; 
+    return histogram;
 }
