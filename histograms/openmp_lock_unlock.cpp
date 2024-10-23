3237b3af43693342a7aa2eb9efcf7eedade9969f
Enmaaaaaa <83886600+Enmaaaaaa@users.noreply.github.com>
2024-10-23 09:13:03 -0500

    Update openmp_lock_unlock.cpp

--- histograms/openmp_lock_unlock.cpp
+++ histograms/openmp_lock_unlock.cpp
@@ -3,14 +3,14 @@
 
 std::vector<int> OpenMPLockUnlock::calculate(const int* input, const int buckets,
                                              const int input_size) {
-  std::vector<int> histograma(buckets, 0);
+  std::vector<int> histogram(buckets, 0);
 
   #pragma omp parallel for
   for(int idx = 0; idx < input_size; idx++) {
     mtx.lock();  
-    histograma[input[idx] - 1]++;
+    histogram[input[idx] - 1]++;
     mtx.unlock();  
   }
 
-  return histograma;
+  return histogram;
 }
